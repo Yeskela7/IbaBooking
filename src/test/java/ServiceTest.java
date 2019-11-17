c5bfe65b21885483e92515f65ad73a779bd65122
Javid <javid.mammadli99@gmail.com>
2019-11-17 20:06:15 +0400

    -- only 2 files

--- src/test/java/ServiceTest.java
+++ src/test/java/ServiceTest.java
@@ -1,4 +1,3 @@
-import booking.service.Client;
 import booking.service.Service;
 import flights.Flight;
 import org.junit.jupiter.api.Test;
@@ -8,31 +7,62 @@ import java.text.ParseException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-//
-//class ServiceTest {
-//
-//    Service service=new Service();
-//
-//
-////    @Test
-////    void equals() {
-//        Client client1=new Client("Dexter","Johnson");
-//        Client client2=new Client("Sasha","Petrov");
-//
-//        assertEquals(false,client1.equals(client2));
-//
-//        Client client3=new Client("Dexter","Johnson");
-//        assertEquals(false,client1.equals(client3));
-//    }
-//
-//    @Test
-//    void get() throws IOException, ClassNotFoundException {
-//        Client client=new Client("Petr","Mitrich");
-//        assertNull(service.service.get(2));
-//    }
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTest {
+
+    Service service=new Service();
 
 
+    @Test
+    void equals() {
+        Client client1=new Client(12,"Dexter","Johnson");
+        Client client2=new Client(13,"Sasha","Petrov");
 
-//}
+        assertEquals(false,client1.equals(client2));
+
+        Client client3=new Client(31,"Dexter","Johnson");
+        assertEquals(false,client1.equals(client3));
+    }
+
+    @Test
+    void get() throws IOException, ClassNotFoundException {
+        Client client=new Client(44,"Petr","Mitrich");
+        assertNull(service.service.get(2));
+
+        service.service.save(client);
+        assertEquals(client,service.service.get(44));
+    }
+
+    @Test
+    void cancelBooking() throws ParseException {
+        Client client = new Client(2,"Javid", "Mammadli");
+        Map<Integer,Client> map=new HashMap<>();
+        map.put(10,client);
+        Long start=new Long(21);
+        Long end=new Long(22);
+
+        Flight flight = new Flight(2, 100, "11:21 01/11/2019", "11:21 21/11/2019", "London", "New-York");
+
+//        assertEquals(false,service.cancelBooking(client,23));
+
+        client.addFlight(flight);
+        assertEquals(true,service.cancelBooking(client,2));
+
+
+        Flight fligh2 = new Flight(3, 111, "11:21 01/11/2019", "11:21 21/11/2019", "London", "New-York");
+        Flight flight3 = new Flight(4, 10, "11:21 01/11/2019", "11:21 21/11/2019", "London", "New-York");
+        Flight flight4 = new Flight(5, 41, "11:21 01/11/2019", "11:21 21/11/2019", "London", "New-York");
+        client.addFlight(flight);
+        client.addFlight(fligh2);
+        client.addFlight(flight3);
+        client.addFlight(flight4);
+
+        assertEquals(true,service.cancelBooking(client,2));
+        assertEquals(true,service.cancelBooking(client,3));
+        assertEquals(false,service.cancelBooking(client,2121));
+        assertEquals(true,service.cancelBooking(client,4));
+        assertEquals(true,service.cancelBooking(client,5));
+    }
+
+}
